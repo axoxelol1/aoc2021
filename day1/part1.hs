8da710fb5e9a0ed2d57e6bc1494f2fa452af168e
Axel Larsson <axoxelol_01@hotmail.com>
2021-12-01 15:43:35 +0100

    Init commit day1 haskell

---
+++ day1/part1.hs
@@ -0,0 +1,6 @@
+module Main where
+
+main :: IO ()
+main = do
+    depths <- map (\x -> read x :: Integer) <$> (lines <$> readFile "input.txt")
+    print $ length $ [x | x <- zipWith (<) depths (drop 1 depths), x]
---
+++ day1/part2.hs
@@ -0,0 +1,10 @@
+module Main where
+
+main :: IO ()
+main = do
+    depths <- map (\x -> read x :: Integer) <$> (lines <$> readFile "input.txt")
+    let blocks = map sum (chunkList depths)
+    print $ length $ [x | x <- zipWith (<) blocks (drop 1 blocks), x]
+
+chunkList [] = []
+chunkList list = take 3 list : chunkList (drop 1 list)
