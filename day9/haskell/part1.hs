8c873d9b5d82c3447c0b9f9b41c6b96407fc7ce7
Axel Larsson <axoxelol_01@hotmail.com>
2021-12-12 22:47:10 +0100

    rewrite part 1

--- day9/haskell/part1.hs
+++ day9/haskell/part1.hs
@@ -1,25 +1,26 @@
 module Main where
 import Data.Char (digitToInt)
 import Data.List (transpose)
+import Data.Maybe (catMaybes)
+
+type Point = (Int,Int)
 
 main :: IO ()
 main = do
     input <- map (map digitToInt) . lines <$> readFile "../input.txt"
-    let lowPoints = [(r, c) | r <- [0..(length input - 1)], c <- [0..(length (head input) - 1)], ltAll r c input]
+    let w = length (head input)
+    let h = length input
+    let lowPoints = [(r, c) | r <- [0..h-1], c <- [0..w-1], isLowPoint input (r,c)]
     print $ sum [input !! fst lp !! snd lp + 1 | lp <- lowPoints]
 
-ltAll :: Int -> Int -> [[Int]] -> Bool
-ltAll r c m = ltLeft r c m && ltRight r c m && ltUp r c m && ltDown r c m
-    where
-        ltLeft r c matrix
-            | (c-1) < 0 = True
-            | otherwise = matrix !! r !! c < matrix !! r !! (c-1)
-        ltRight r c matrix
-            | c+2 > length (matrix !! r) = True
-            | otherwise = matrix !! r !! c < matrix !! r !! (c+1)
-        ltUp r c matrix
-            | (r-1) < 0 = True
-            | otherwise = matrix !! r !! c < matrix !! (r-1) !! c
-        ltDown r c matrix
-            | r+2 > length matrix = True
-            | otherwise = matrix !! r !! c < matrix !! (r+1) !! c
+isLowPoint :: Ord a => [[a]] -> Point -> Bool
+isLowPoint m (r,c) = all (> (m !! r !! c)) (catMaybes [m !!! coords | coords <- neighbours (r,c)])
+
+(!!!) :: [[a]] -> Point -> Maybe a
+m !!! (r,c)
+    | r < 0 || r >= length m = Nothing
+    | c < 0 || c >= length (head m) = Nothing
+    | otherwise = Just $ m !! r !! c
+
+neighbours :: Point -> [Point]
+neighbours (r,c) = [(r,c-1),(r,c+1),(r-1,c),(r+1,c)]
